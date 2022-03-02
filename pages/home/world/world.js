b069a7a1db2df287c943d109f634d5d9f318dd1f
unknown <unknown@example.com>
2022-03-02 16:31:18 +0800

    Initial Commit

---
+++ pages/home/world/world.js
@@ -0,0 +1,15 @@
+const app = getApp()
+Page({
+  data:{
+    unions:{
+      unionImg:"https://static.zhihu.com/heifetz/favicon.ico",
+      unionName:"test",
+    },
+    stickyTestImg:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fblog%2F202107%2F05%2F20210705100427_ee4b8.thumb.1000_0.jpg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1628415177&t=87962cc902fb5925da0a4d60d4c48ca9",
+    contentSheet:{
+      position:"fixed",
+      overflowY:"hidden",
+      height:"",
+    },
+  },
+})
